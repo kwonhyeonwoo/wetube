2754eaa86b753cde3bd1059f1d4b69b17f09d05c
keonhyeonwoo <khw19980926@gmail.com>
2026-01-13 00:16:12 +0900

    기본 세팅 및 연습

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from "express";
 const app = express();
 const port = 3000;
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Express with TypeScript!');
+    console.log('hello express ! lets go home!')
 });
 
 app.listen(port, () => {
